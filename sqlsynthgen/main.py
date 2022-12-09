c5a0fbe4c4b0dce066f71b54c38773a38297bf1f
Iain <25081046+Iain-S@users.noreply.github.com>
2022-12-09 18:34:47 +0000

    Generate one row for the AdvanceDecision table

--- sqlsynthgen/main.py
+++ sqlsynthgen/main.py
@@ -1,13 +1,33 @@
-from sqlalchemy import create_engine
+"""Entrypoint for the sqlsynthgen package."""
+from sqlalchemy import create_engine, insert
 
 from sqlsynthgen.settings import Settings
-from sqlsynthgen.star import metadata
+from sqlsynthgen.star import AdvanceDecision, metadata
+from sqlsynthgen.star_gens import AdvanceDecisionGenerator
+
+settings = Settings()
 
 
 def main():
-    settings = Settings()
+    """Create an empty schema and populate it with dummy data."""
     engine = create_engine(settings.postgres_dsn)
+    populate(engine)
+
+    return
     metadata.create_all(bind=engine)
+
+
+def populate(engine):
+    """Populate a database schema with dummy data."""
+    # for table in metadata.sorted_tables:
+    # print(dir(table))
+    # print(table.name)
+    # print(table.columns[0].type)
+    # return
+
+    stmt = insert(AdvanceDecision).values(AdvanceDecisionGenerator().__dict__)
+    with engine.connect() as conn:
+        conn.execute(stmt)
 
 
 if __name__ == "__main__":
