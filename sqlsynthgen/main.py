1f3ce14fe67f4321af03c1f60ad09e0ce80a3583
Iain <25081046+Iain-S@users.noreply.github.com>
2022-12-14 12:19:06 +0000

    Fix mypy errors

--- sqlsynthgen/main.py
+++ sqlsynthgen/main.py
@@ -1,5 +1,8 @@
 """Entrypoint for the sqlsynthgen package."""
+from typing import Any
+
 from sqlalchemy import create_engine, insert
+from sqlalchemy.engine.base import Engine
 
 from sqlsynthgen.settings import Settings
 from sqlsynthgen.star import AdvanceDecision, metadata
@@ -8,7 +11,7 @@ from sqlsynthgen.star_gens import AdvanceDecisionGenerator
 settings = Settings()
 
 
-def main():
+def main() -> None:
     """Create an empty schema and populate it with dummy data."""
     engine = create_engine(settings.postgres_dsn)
     populate(engine)
@@ -17,7 +20,7 @@ def main():
     metadata.create_all(bind=engine)
 
 
-def populate(engine):
+def populate(engine: Any) -> None:
     """Populate a database schema with dummy data."""
     # for table in metadata.sorted_tables:
     # print(dir(table))
