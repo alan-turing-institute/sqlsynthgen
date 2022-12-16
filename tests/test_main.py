b220b98f7823a824a345c400a1bf96a691a53755
Iain <25081046+Iain-S@users.noreply.github.com>
2022-12-16 15:22:21 +0000

    Mock get_settings so tests don't need env vars

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,8 +1,21 @@
 """Tests for the main module."""
+from functools import lru_cache
 from unittest import TestCase
 from unittest.mock import patch
 
-from sqlsynthgen import main
+from sqlsynthgen import main, settings
+
+
+@lru_cache(1)
+def get_test_settings():
+    """Get a Settings object that ignores .env files and environment variables."""
+    return settings.Settings(
+        db_host_name="db_host_name",
+        db_user_name="db_user_name",
+        db_password="db_password",
+        db_name="db_name",
+        _env_file=None,
+    )
 
 
 class MyTestCase(TestCase):
@@ -10,7 +23,10 @@ class MyTestCase(TestCase):
 
     def test_main(self) -> None:
         """Check that the main function works."""
-        with patch("sqlsynthgen.main.populate") as mock_populate:
+        with patch("sqlsynthgen.main.populate") as mock_populate, patch(
+            "sqlsynthgen.main.get_settings"
+        ) as mock_get_settings:
+            mock_get_settings.return_value = get_test_settings()
             main.main()
 
         mock_populate.assert_called_once()
