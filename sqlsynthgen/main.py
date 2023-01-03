3f01dca9a83a8445d982f8e2022a6adeb8a201fe
Iain <25081046+Iain-S@users.noreply.github.com>
2023-01-03 15:49:43 +0000

    Fix linting errors

--- sqlsynthgen/main.py
+++ sqlsynthgen/main.py
@@ -3,40 +3,37 @@ from typing import Any
 
 from sqlalchemy import create_engine, insert
 
-from sqlsynthgen.create_generators import create_generators_from_tables
 from sqlsynthgen.settings import get_settings
 
-# TODO Fix the below imports
-from tests.examples.example_tables import metadata
-from tests.examples.expected_output import sorted_generators as test_generators
 
-# from sqlsynthgen.star import AdvanceDecision, metadata
-# from sqlsynthgen.star_gens import AdvanceDecisionGenerator
+def create_tables(metadata: Any) -> Any:
+    """Create tables described by the sqlalchemy metadata object."""
+    settings = get_settings()
+    engine = create_engine(settings.postgres_dsn)
+    metadata.create_all(engine)
 
 
 def main() -> None:
-    """Create an empty schema and populate it with dummy data."""
+    """Not implemented yet."""
+    raise NotImplementedError
 
+
+def generate(sorted_tables: list, sorted_generators: list) -> Any:
+    """Connect to a database and"""
     settings = get_settings()
     engine = create_engine(settings.postgres_dsn)
-    populate(engine)
-    # metadata.create_all(bind=engine)
+
+    with engine.connect() as conn:
+        populate(conn, sorted_tables, sorted_generators)
 
 
-def populate(engine: Any) -> None:
+def populate(conn: Any, tables: list, generators: list) -> None:
     """Populate a database schema with dummy data."""
-    # for table in metadata.sorted_tables:
-    # print(dir(table))
-    # print(table.name)
-    # print(table.columns[0].type)
-    # return
 
-    for table, generator in zip(metadata.sorted_tables, test_generators):
-        with engine.connect() as conn:
-            stmt = insert(table).values(generator(conn).__dict__)
-            conn.execute(stmt)
+    for table, generator in zip(tables, generators):
+        stmt = insert(table).values(generator(conn).__dict__)
+        conn.execute(stmt)
 
 
 if __name__ == "__main__":
-    # main()
-    create_generators_from_tables("sqlsynthgen.star")
+    main()
