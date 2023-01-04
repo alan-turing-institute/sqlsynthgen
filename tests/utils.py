4a4c850e66b43c04aaea093af698fe066063f860
Iain <25081046+Iain-S@users.noreply.github.com>
2023-01-04 17:07:05 +0000

    Implement make-tables command

--- tests/utils.py
+++ tests/utils.py
@@ -7,10 +7,14 @@ from sqlsynthgen import settings
 @lru_cache(1)
 def get_test_settings() -> settings.Settings:
     """Get a Settings object that ignores .env files and environment variables."""
+
     return settings.Settings(
-        db_host_name="db_host_name",
-        db_user_name="db_user_name",
-        db_password="db_password",
-        db_name="db_name",
+        src_host_name="shost",
+        src_user_name="suser",
+        src_password="spassword",
+        dst_host_name="dhost",
+        dst_user_name="duser",
+        dst_password="dpassword",
+        # To stop any local .env files influencing the test
         _env_file=None,
     )
