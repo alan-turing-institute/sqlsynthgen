1b1f9b0be16a62ad149c7c0dc2201c69f42633cd
myyong <myong@turing.ac.uk>
2023-01-16 14:15:34 +0000

    Add path to code

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -6,6 +6,10 @@
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
+import os
+import sys
+sys.path.insert(0, os.path.abspath('../../sqlsynthgen/'))
+
 project = 'sqlsynthgen'
 copyright = '2023, anon'
 author = 'anon'
@@ -31,3 +35,4 @@ language = 'Python'
 
 html_theme = 'alabaster'
 html_static_path = ['_static']
+
