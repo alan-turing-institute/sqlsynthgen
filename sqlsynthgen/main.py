314451c2d02387a331828090ca1617a10c75fba8
Iain <25081046+Iain-S@users.noreply.github.com>
2023-02-28 14:19:27 +0000

    Silence STDERR output from functional tests.
    And correct some docstrings.

--- sqlsynthgen/main.py
+++ sqlsynthgen/main.py
@@ -133,7 +133,7 @@ def make_tables() -> None:
     """Make a SQLAlchemy file of Table classes.
 
     This CLI command deploys sqlacodegen to discover a
-    schema structure, and generates a object relational model declared
+    schema structure, and generates an object relational model declared
     as Python classes.
 
     Example:
