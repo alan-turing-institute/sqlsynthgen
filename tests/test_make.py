0165ba9a4967287cd33d24f7397f5defb67d437d
Iain <25081046+Iain-S@users.noreply.github.com>
2023-03-31 13:03:05 +0100

    Fix unit tests

--- tests/test_make.py
+++ tests/test_make.py
@@ -2,19 +2,21 @@
 import os
 from io import StringIO
 from pathlib import Path
-from subprocess import CalledProcessError
-from unittest.mock import MagicMock, call, patch
+from unittest.mock import MagicMock, patch
 
 import yaml
 
-from sqlsynthgen import make
-from sqlsynthgen.make import make_tables_file
+from sqlsynthgen.make import (
+    make_generators_from_tables,
+    make_src_stats,
+    make_tables_file,
+)
 from tests.examples import example_orm
-from tests.utils import SSGTestCase, SysExit
+from tests.utils import RequiresDBTestCase, SSGTestCase
 
 
-class TestMake(SSGTestCase):
-    """Tests that don't require a database."""
+class TestMakeGenerators(SSGTestCase):
+    """Test the make_generators_from_tables function."""
 
     test_dir = Path("tests/examples")
     start_dir = os.getcwd()
@@ -41,88 +43,99 @@ class TestMake(SSGTestCase):
             config = yaml.safe_load(f)
         stats_path = "example_stats.yaml"
 
-        actual = make.make_generators_from_tables(example_orm, config, stats_path)
+        actual = make_generators_from_tables(example_orm, config, stats_path)
         mock_download.assert_called_once()
         mock_create.assert_called_once()
 
         self.assertEqual(expected, actual)
 
-    @patch("sqlsynthgen.make.run")
-    def test_make_tables_file(self, mock_run: MagicMock) -> None:
+
+class TestMakeTables(SSGTestCase):
+    """Test the make_tables function."""
+
+    test_dir = Path("tests/examples")
+    start_dir = os.getcwd()
+
+    def setUp(self) -> None:
+        """Pre-test setup."""
+        os.chdir(self.test_dir)
+
+    def tearDown(self) -> None:
+        """Post-test cleanup."""
+        os.chdir(self.start_dir)
+
+    @patch("sqlsynthgen.make.MetaData")
+    @patch("sqlsynthgen.make.entry_points")
+    def test_make_tables_file(self, mock_entry: MagicMock, _: MagicMock) -> None:
         """Test the make_tables_file function."""
-
-        mock_run.return_value.stdout = "some output"
-
-        make_tables_file("my:postgres/db", None)
+        mock_ep = MagicMock()
+        mock_ep.name = "declarative"
+        mock_ep.load.return_value.return_value.generate.return_value = (
+            "some generated code"
+        )
+        mock_entry.return_value = [mock_ep]
 
         self.assertEqual(
-            call(
-                [
-                    "sqlacodegen",
-                    "my:postgres/db",
-                ],
-                capture_output=True,
-                encoding="utf-8",
-                check=True,
-            ),
-            mock_run.call_args_list[0],
+            "some generated code",
+            make_tables_file("postgresql://postgres@1.2.3.4/db", None),
         )
 
-    @patch("sqlsynthgen.make.run")
-    def test_make_tables_file_with_schema(self, mock_run: MagicMock) -> None:
+    @patch("sqlsynthgen.make.MetaData")
+    @patch("sqlsynthgen.make.entry_points")
+    def test_make_tables_file_with_schema(
+        self, mock_entry: MagicMock, _: MagicMock
+    ) -> None:
         """Check that the function handles the schema setting."""
 
-        make_tables_file("my:postgres/db", "my_schema")
+        mock_ep = MagicMock()
+        mock_ep.name = "declarative"
+        mock_ep.load.return_value.return_value.generate.return_value = (
+            "some generated code"
+        )
+        mock_entry.return_value = [mock_ep]
 
         self.assertEqual(
-            call(
-                [
-                    "sqlacodegen",
-                    "--schema=my_schema",
-                    "my:postgres/db",
-                ],
-                capture_output=True,
-                encoding="utf-8",
-                check=True,
-            ),
-            mock_run.call_args_list[0],
+            "some generated code",
+            make_tables_file("postgresql://postgres@1.2.3.4/db", "myschema"),
         )
 
-    @patch("sys.exit")
+    @patch("sqlsynthgen.make.MetaData")
     @patch("sqlsynthgen.make.stderr", new_callable=StringIO)
-    @patch("sqlsynthgen.make.run")
-    def test_make_tables_handles_errors(
-        self, mock_run: MagicMock, mock_stderr: MagicMock, mock_exit: MagicMock
-    ) -> None:
-        """Test the make-tables sub-command handles sqlacodegen errors."""
-
-        mock_run.side_effect = CalledProcessError(
-            returncode=99, cmd="some-cmd", stderr="some-error-output"
-        )
-        mock_exit.side_effect = SysExit
-
-        try:
-            make_tables_file("my:postgres/db", None)
-        except SysExit:
-            pass
-
-        mock_exit.assert_called_once_with(99)
-        self.assertEqual("some-error-output\n", mock_stderr.getvalue())
-
-    @patch("sqlsynthgen.make.stderr", new_callable=StringIO)
-    @patch("sqlsynthgen.make.run")
+    @patch("sqlsynthgen.make.entry_points")
     def test_make_tables_warns_no_pk(
-        self, mock_run: MagicMock, mock_stderr: MagicMock
+        self, mock_entry: MagicMock, mock_stderr: MagicMock, _: MagicMock
     ) -> None:
         """Test the make-tables sub-command warns about Tables()."""
 
-        mock_run.return_value.stdout = "t_nopk_table = Table("
-        make_tables_file("my:postgres/db", None)
+        mock_ep = MagicMock()
+        mock_ep.name = "declarative"
+        mock_entry.return_value = [mock_ep]
+
+        mock_ep.load.return_value.return_value.generate.return_value = (
+            "t_nopk_table = Table("
+        )
+
+        make_tables_file("postgresql://postgres@127.0.0.1:5432", None)
 
         self.assertEqual(
             "WARNING: Table without PK detected. sqlsynthgen may not be able to continue.\n",
             mock_stderr.getvalue(),
         )
+
+
+class TestMakeStats(RequiresDBTestCase):
+    """Test the make_src_stats function."""
+
+    test_dir = Path("tests/examples")
+    start_dir = os.getcwd()
+
+    def setUp(self) -> None:
+        """Pre-test setup."""
+        os.chdir(self.test_dir)
+
+    def tearDown(self) -> None:
+        """Post-test cleanup."""
+        os.chdir(self.start_dir)
 
     def test_make_stats(self) -> None:
         """Test the make_src_stats function."""
@@ -130,11 +143,19 @@ class TestMake(SSGTestCase):
         conf_path = Path("example_config.yaml")
         with open(conf_path, "r", encoding="utf8") as f:
             config = yaml.safe_load(f)
-        src_stats = make.make_src_stats(connection_string, config)
-        self.assertSetEqual({"count_opt_outs"}, set(src_stats.keys()))
-        count_opt_outs = src_stats["count_opt_outs"]
-        self.assertEqual(len(count_opt_outs), 2)
-        self.assertIsInstance(count_opt_outs[0][0], int)
-        self.assertIs(count_opt_outs[0][1], False)
-        self.assertIsInstance(count_opt_outs[1][0], int)
-        self.assertIs(count_opt_outs[1][1], True)
+
+        # Check that make_src_stats works with, or without, a schema
+        for args in (
+            (connection_string, config),
+            (connection_string, config, "public"),
+        ):
+
+            src_stats = make_src_stats(*args)
+
+            self.assertSetEqual({"count_opt_outs"}, set(src_stats.keys()))
+            count_opt_outs = src_stats["count_opt_outs"]
+            self.assertEqual(len(count_opt_outs), 2)
+            self.assertIsInstance(count_opt_outs[0][0], int)
+            self.assertIs(count_opt_outs[0][1], False)
+            self.assertIsInstance(count_opt_outs[1][0], int)
+            self.assertIs(count_opt_outs[1][1], True)
