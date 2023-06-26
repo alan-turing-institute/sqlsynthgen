7f523992b206861072cb1736fe21cb0a9952f9c6
Markus Hauru <mhauru@turing.ac.uk>
2023-06-26 10:57:47 +0100

    More error checking in base.py

--- sqlsynthgen/base.py
+++ sqlsynthgen/base.py
@@ -1,10 +1,12 @@
 """Base table generator classes."""
+import logging
 from dataclasses import dataclass
 from pathlib import Path
 from typing import Any
 
 import yaml
 from sqlalchemy import insert
+from sqlalchemy.exc import SQLAlchemyError
 
 
 @dataclass
@@ -15,10 +17,25 @@ class FileUploader:
 
     def load(self, connection: Any) -> None:
         """Load the data from file."""
-        with Path(self.table.fullname + ".yaml").open(
-            "r", newline="", encoding="utf-8"
-        ) as yamlfile:
-            rows = yaml.load(yamlfile, Loader=yaml.Loader)
-            if rows:
-                stmt = insert(self.table).values(list(rows))
-                connection.execute(stmt)
+        yaml_file = Path(self.table.fullname + ".yaml")
+        if not yaml_file.exists():
+            logging.warning("File %s not found. Skipping...", yaml_file)
+            return
+        try:
+            with yaml_file.open("r", newline="", encoding="utf-8") as yamlfile:
+                rows = yaml.safe_load(yamlfile)
+        except yaml.YAMLError as e:
+            logging.warning("Error reading YAML file %s: %s", yaml_file, e)
+            return
+
+        if not rows:
+            logging.warning("No rows in %s. Skipping...", yaml_file)
+            return
+
+        try:
+            stmt = insert(self.table).values(list(rows))
+            connection.execute(stmt)
+        except SQLAlchemyError as e:
+            logging.warning(
+                "Error inserting rows into table %s: %s", self.table.fullname, e
+            )
