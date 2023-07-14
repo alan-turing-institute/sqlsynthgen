6cb29d3fbd05596c765ccbe566f89c56ee95e761
Olajumoke <oolatunji@turing.ac.uk>
2023-07-14 17:27:46 +0100

    Edited the tutorials with Carlos' changes and added the completed files in airbnb

--- tests/examples/airbnb/airbnb_generators.py
+++ tests/examples/airbnb/airbnb_generators.py
@@ -1,8 +1,6 @@
-import datetime
 import random
+import datetime
 from typing import Optional, Generator, Tuple
-
-
 
 def user_dates_provider(generic):
     date_account_created: datetime.date = generic.datetime.date(start=2010, end=2015)
@@ -11,26 +9,45 @@ def user_dates_provider(generic):
     if generic.choice([True, False]):
         booking_date = generic.datetime.date(
             start=date_account_created.year + 1, end=2016
-        )
+         )
 
     return date_account_created, booking_date
-
 
 def user_age_provider(query_results):
     mu: float = query_results[0][0]
     sigma: float = query_results[0][1]
-
     return random.gauss(mu, sigma)
 
+def sessions_story():
+    """Generate users and their sessions."""
+    device_types = ["Mac Desktop", "Windows Desktop", "iPhone"]
 
-def session_story() -> Generator[Tuple[str, dict], dict, None]:
-    user: dict = yield ("users", {})  # the table name  # 0 or more column values
+    # a new user will be sent back to us with our randomly chosen device type
+    user: dict = yield (
+        "users",  # table name
+        {
+            "first_device_type": random.choice(device_types)
+        }  # see 1. below
+    )
+
+    # create between 10 and 19 sessions per user
     sessions_per_user: int = random.randint(10, 20)
 
     for _ in range(sessions_per_user):
         if random.random() < 0.8:
             # most often, the session is from the user's sign-up device...
-            yield ("sessions", {"device_type": user["first_device_type"]})
+            yield (
+                "sessions",
+                {
+                    "user_id": user["id"],  # see 2. below
+                    "device_type": user["first_device_type"],
+                }
+            )
         else:
             # ...but sometimes it is from any device type
-            yield "sessions", {}
+            yield (
+                "sessions",
+                {
+                    "user_id": user["id"],
+                    "device_type": random.choice(device_types)},
+            )
